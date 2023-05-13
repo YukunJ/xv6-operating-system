cad313ec6df40529d8f117cc1eaf199a10081269
YukunJ <yukunj@andrew.cmu.edu>
2023-05-13 13:38:14 -0400

    feat: uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,47 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for user-level thread switching
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct thread_context     thread_context;    /* register status */
+  char                      stack[STACK_SIZE]; /* the thread's stack */
+  int                       state;             /* FREE, RUNNING, RUNNABLE */
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
+
+/*
+ * helper function to setup the routine for a newly-created thread
+ */
+void clear_thread(struct thread *t, void (*func)()) {
+  memset((void *)&t->stack, 0, STACK_SIZE);
+  memset((void *)&t->thread_context, 0, sizeof(struct thread_context));
+  t->state = RUNNABLE;
+  t->thread_context.sp = (uint64) ((char *)&t->stack + STACK_SIZE);
+  t->thread_context.ra = (uint64) func;
+}
+
 void 
 thread_init(void)
 {
@@ -63,6 +94,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64) t, (uint64) current_thread);
   } else
     next_thread = 0;
 }
@@ -75,8 +107,8 @@ thread_create(void (*func)())
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
-  t->state = RUNNABLE;
   // YOUR CODE HERE
+  clear_thread(t, func);
 }
 
 void 
